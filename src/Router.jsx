495c889fa4aa57fcae5e9a6a02360519a8c9c9d4
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-11 14:49:06 -0300

    feat: add signup route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Hashtag from "./pages/hashtag/Hashtag";
 import Home from "./pages/home/Home";
+import Signup from "./pages/sign/Signup";
 
 export default function Router() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
+        <Routes>    
+            <Route path="/" element={<Signup />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/hashtag/:hashtag" element={<Hashtag />} />
         </Routes>
     )
