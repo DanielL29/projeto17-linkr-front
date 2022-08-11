b7ebd6ad20c17066f155af870eadcfa2164de3d7
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-11 19:18:24 -0300

    fix: route name

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,7 +6,7 @@ import Signup from "./pages/sign/Signup";
 export default function Router() {
     return (
         <Routes>    
-            <Route path="/" element={<Signup />} />
+            <Route path="/sign-up" element={<Signup />} />
             <Route path="/home" element={<Home />} />
             <Route path="/hashtag/:hashtag" element={<Hashtag />} />
         </Routes>
