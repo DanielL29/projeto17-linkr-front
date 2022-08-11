6b4acc4b3e7c2602a99b882437fdf628f8de1640
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-08-10 23:18:51 -0300

    fix: fixing header, adding page not found

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -1,11 +1,32 @@
 import ToggleLogout from "../toggle-logout/ToggleLogout";
-import { HeaderWrapper } from "./HeaderStyle";
+import { HeaderWrapper, PageNotFound } from "./HeaderStyle";
+import { useNavigate, useMatch } from 'react-router-dom';
+import brokenLink from '../../assets/images/broken-link.png'
 
 export default function Header() {
+    const navigate = useNavigate()
+    const matchHashtag = useMatch('/hashtag/:hashtag')
+    const matchHome = useMatch('/home')
+
+    function RenderHeader() {
+        if (matchHashtag || matchHome) {
+            return (
+                <HeaderWrapper>
+                    <h1 onClick={() => navigate('/')}>linkr</h1>
+                    <ToggleLogout />
+                </HeaderWrapper>
+            )
+        }
+
+        return (
+            <PageNotFound>
+                <img src={brokenLink} alt="page not found" />
+                <h1>Oops! this page doesn't exists</h1>
+            </PageNotFound>
+        )
+    }
+
     return (
-        <HeaderWrapper>
-            <h1>linkr</h1>
-            <ToggleLogout />
-        </HeaderWrapper>
+        <RenderHeader />
     )
 }
