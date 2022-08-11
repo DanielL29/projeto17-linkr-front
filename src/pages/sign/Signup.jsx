8b190957e9963eccac63b1c588a7000ff273b3f9
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-11 19:18:38 -0300

    feat: signup page

--- src/pages/sign/Signup.jsx
+++ src/pages/sign/Signup.jsx
@@ -1,18 +1,45 @@
-import { Container, PresentationContainer, SignupContainer } from "./style";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Container, PresentationContainer, SignupContainer} from "./style";
+import { ThreeDots } from "react-loader-spinner";
+import { signup } from "../../services/authService";
+import { ToastContainer } from "react-toastify";
 
 export default function Signup() {
+    const navigate = useNavigate();
+    const [load, setLoad] = useState(false);
+    const [newUser, setNewUser] = useState({
+        email: "",
+        password: "",
+        username: "",
+        pictureUrl: "",
+    });
+    function handleSubmit(e) {
+        e.preventDefault();
+        setLoad(true);
+        try {
+            signup(newUser);
+            setLoad(false);
+            navigate("/", { replace: true });
+        } catch {
+            setLoad(false);
+        }
+    }
+
     return(
         <Container>
+            <ToastContainer />
             <PresentationContainer>
                 <h1>linkr</h1>
                 <h2>save, share and discover<br/>the best links on the web</h2>
             </PresentationContainer>
-            <SignupContainer>
-                <input type="email" placeholder="e-mail" />
-                <input type="password" placeholder="password" />
-                <input type="text" placeholder="username" />
-                <input type="url" placeholder="picture url" />
-                <input type="button"/>
+            <SignupContainer onSubmit={handleSubmit} load={load} >
+                <input type="email" placeholder="e-mail" required disabled={load} value={newUser.email} onChange={(e) => setNewUser({...newUser, email: e.target.value})} />
+                <input type="password" placeholder="password" required disabled={load} value={newUser.password} onChange={(e) => setNewUser({...newUser, password: e.target.value})} />
+                <input type="text" placeholder="username" required disabled={load} value={newUser.username} onChange={(e) => setNewUser({...newUser, username: e.target.value})} />
+                <input type="url" placeholder="picture url" required disabled={load} value={newUser.pictureUrl} onChange={(e) => setNewUser({...newUser, pictureUrl: e.target.value})} />
+                <button type="submit" disabled={load}>{load ? <ThreeDots color="#FFFFFF" height={80} width={80} /> : "Sign Up"}</button>
+                <Link to="/" placeholder="">Switch back to log in</Link>
             </SignupContainer>
         </Container>
     );
--- src/pages/sign/style.jsx
+++ src/pages/sign/style.jsx
@@ -5,7 +5,6 @@ const Container = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
-    justify-content: space-between;
 `;
 
 const PresentationContainer = styled.div`
@@ -16,6 +15,7 @@ const PresentationContainer = styled.div`
     justify-content: center;
     flex-direction: column;
     padding-left: 11rem;
+    box-shadow: 4px 0px 4px rgba(0, 0, 0, 0.25);
     h1 {
         font-family: "Passion One";
         font-size: 106px;
@@ -34,6 +34,36 @@ const PresentationContainer = styled.div`
 const SignupContainer = styled.form`
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 48%;
+    width: 40%;
+    input, button {
+        width: calc(45% + 150px);
+        height: 65px;
+        background-color: #FFFFFF;
+        border-radius: 6px;
+        font-family: "Oswald";
+        font-size: 27px;
+        color: #9F9F9F;
+        padding-left: 15px;
+        opacity: ${props => props.load ? 0.6 : 1};
+    }
+    button {
+        background-color: #1877F2;
+        border: none;
+        padding-left: 0;
+        color: #FFFFFF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    a {
+        color: #FFFFFF;
+        font-size: 20px;
+        opacity: ${props => props.load ? 0.6 : 1};
+    }
 `;
 
-export { Container ,PresentationContainer, SignupContainer };
+
+export { Container, PresentationContainer, SignupContainer};
