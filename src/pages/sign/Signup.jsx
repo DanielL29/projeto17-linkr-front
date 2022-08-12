41a215fd5f4d753974c944ea6191b2dbae27efed
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-12 12:02:52 -0300

    fix: treat errors were not stopping navigate

--- src/pages/sign/Signup.jsx
+++ src/pages/sign/Signup.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Container, PresentationContainer, SignupContainer} from "./style";
+import { Container, PresentationContainer, SignContainer} from "./style";
 import { ThreeDots } from "react-loader-spinner";
 import { signup } from "../../services/authService";
 import { ToastContainer } from "react-toastify";
+import { treatErrors } from "../../utils/global";
 
 export default function Signup() {
     const navigate = useNavigate();
@@ -14,15 +15,17 @@ export default function Signup() {
         username: "",
         pictureUrl: "",
     });
-    function handleSubmit(e) {
+    
+    async function handleSignup(e) {
         e.preventDefault();
         setLoad(true);
         try {
-            signup(newUser);
+            await signup(newUser);
             setLoad(false);
             navigate("/", { replace: true });
-        } catch {
+        } catch(err) {
             setLoad(false);
+            treatErrors(err);
         }
     }
 
@@ -33,14 +36,14 @@ export default function Signup() {
                 <h1>linkr</h1>
                 <h2>save, share and discover<br/>the best links on the web</h2>
             </PresentationContainer>
-            <SignupContainer onSubmit={handleSubmit} load={load} >
+            <SignContainer onSubmit={handleSignup} load={load} >
                 <input type="email" placeholder="e-mail" required disabled={load} value={newUser.email} onChange={(e) => setNewUser({...newUser, email: e.target.value})} />
                 <input type="password" placeholder="password" required disabled={load} value={newUser.password} onChange={(e) => setNewUser({...newUser, password: e.target.value})} />
                 <input type="text" placeholder="username" required disabled={load} value={newUser.username} onChange={(e) => setNewUser({...newUser, username: e.target.value})} />
                 <input type="url" placeholder="picture url" required disabled={load} value={newUser.pictureUrl} onChange={(e) => setNewUser({...newUser, pictureUrl: e.target.value})} />
                 <button type="submit" disabled={load}>{load ? <ThreeDots color="#FFFFFF" height={80} width={80} /> : "Sign Up"}</button>
-                <Link to="/" placeholder="">Switch back to log in</Link>
-            </SignupContainer>
+                <Link to="/">Switch back to log in</Link>
+            </SignContainer>
         </Container>
     );
 }
