7b379727e7c516489d7445d14f9d23d35758e456
Pedro H <driverique@gmail.com>
2022-08-11 22:13:08 -0300

    Fixing: user page

--- src/pages/userPage/UserPage.jsx
+++ src/pages/userPage/UserPage.jsx
@@ -1,41 +1,17 @@
 import "react-toastify/dist/ReactToastify.css";
-import axios from "axios";
 import Timeline from "../../layouts/timeline/Timeline";
 import Trending from "../../layouts/trending/Trending";
 import { Container } from "../home/HomeStyle";
 import { ToastContainer } from "react-toastify";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../../constants";
 
 export default function UserPage() {
   const { id } = useParams();
-  const [userData, setUserData] = useState([{}]);
 
-  useEffect(() => {
-    const promise = axios.get(`${BASE_URL}/user/${id}`);
-
-    promise.then((response) => {
-      setUserData(response.data);
-    });
-    promise.catch((err) => {
-      console.log(err);
-    });
-  }, [id]);
-  
   return (
     <Container>
-      {userData.length !== 0 ? (
-        <div>
-          <Timeline title={`${userData[0].username}'s Posts`} username={id} />
-          <Trending />
-        </div>
-      ) : (
-        <div> 
-            <span>Este usuário não tem nenhum post...</span>
-        </div>
-      )}
-
+        <Timeline title={"Username's Posts"} username={id} />
+        <Trending />
       <ToastContainer />
     </Container>
   );
