b9ffb3235869cb846478978440e92246c656ae30
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-12 12:02:08 -0300

    feat: add route signin

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,10 +3,12 @@ import Hashtag from "./pages/hashtag/Hashtag";
 import Home from "./pages/home/Home";
 import UserPage from "./pages/userPage/UserPage";
 import Signup from "./pages/sign/Signup";
+import Signin from "./pages/sign/Signin";
 
 export default function Router() {
     return (
         <Routes>
+            <Route path="/" element={<Signin />} />
             <Route path="/home" element={<Home />} />
             <Route path="/sign-up" element={<Signup />} />
             <Route path="/hashtag/:hashtag" element={<Hashtag />} />
